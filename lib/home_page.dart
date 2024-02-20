50aca0656599685dfb50928bfae72a9b41c40412
sangik <sang@sellmate.co.kr>
2024-02-20 17:02:01 +0900

    예제 엔티티 추가

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sang/view_model/home_page_view_model.dart';
 import 'package:sang/widget/appbar_widget.dart';
-import 'package:sang/widget/badge_widget.dart';
-import 'package:sang/widget/badge_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppbarWidget(),
-      body: const Center(
-        child: SizedBox(
-          width: 108,
-          child: Text(""),
+      body: Center(
+        child: ChangeNotifierProvider<HomePageViewModel>(
+          create: (_) => HomePageViewModel(),
+          child: SizedBox(
+            width: 108,
+            child: Text(""),
+          ),
         ),
       ),
     );
---
+++ lib/view_model/home_page_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sang/entity/plan_data_entity.dart';
+
+class HomePageViewModel with ChangeNotifier {
+  List<PlanDataEntity> plans = [];
+}
